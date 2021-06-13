9d5bc249b5899001c1bf61f02c9924626ccd7dff
Andrea Whitmer <andrea@nutsandboltsmedia.com>
2021-06-13 08:19:46 -0500

    Update default-image-for-image-field.php

--- default-image-for-image-field.php
+++ default-image-for-image-field.php
@@ -1,7 +1,7 @@
 <?php 
   
   // add default image setting to ACF image fields
-  // let's you select a defualt image
+  // lets you select a default image
   // this is simply taking advantage of a field setting that already exists
   
 	add_action('acf/render_field_settings/type=image', 'add_default_value_to_image_field');
@@ -12,6 +12,15 @@
 			'type'			=> 'image',
 			'name'			=> 'default_value',
 		));
+	}
+
+	// Display default on front end if not replaced
+	add_filter('acf/load_value/type=image', 'reset_default_image', 10, 3);
+	function reset_default_image($value, $post_id, $field) {
+  		if (!$value) {
+    		$value = $field['default_value'];
+  		}
+  		return $value;
 	}
   
 ?>
